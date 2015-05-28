c23b310eb0d2cdcf3b5d01af03e14929188e08c8
Vijay Rajanna <vijayrajanna@vijays-mbp.somers.ibm.com>
2015-05-28 10:46:31 -0400

    Initial Commit

---
+++ LearnObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearnObjectiveC
+//
+//  Created by Vijay Rajanna on 5/28/15.
+//  Copyright (c) 2015 Vijay Rajanna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
